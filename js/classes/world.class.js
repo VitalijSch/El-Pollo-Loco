f0be6c0b71ed87ccc846fe65fc9177fb47f8a303
Vitalij Schwab <schwab.vitalij@gmail.com>
2024-04-25 22:47:18 +0200

    draw first images from character and enemies

--- js/classes/world.class.js
+++ js/classes/world.class.js
@@ -5,9 +5,21 @@ class World {
         new Chicken(),
         new Chicken(),
     ];
+    ctx;
 
-    
+
+    constructor(canvas) {
+        this.ctx = canvas.getContext('2d');
+        this.draw();
+    }
+
+
     draw() {
-
+        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
+        for (let i = 0; i < this.enemies.length; i++) {
+            const enemie = this.enemies[i];
+            const xPos = 390 + i * 100;
+            this.ctx.drawImage(enemie.img, xPos, 300, 100, 150);
+        }
     }
 }

df09805117e58690cf84563c87f66e2dbee73952
Vitalij Schwab <schwab.vitalij@gmail.com>
2024-04-25 22:20:02 +0200

    create classes and general logic

--- js/classes/movable_object.class.js
+++ js/classes/movable_object.class.js
@@ -1,9 +1,21 @@
 class MovableObject {
-    x;
-    y;
+    x = 120;
+    y = 400;
     img;
 
+
+    loadImage(path) {
+        this.img = new Image();
+        this.img.src = path;
+    }
+
+
     moveRight() {
-        console.log('Moving right');
+
+    }
+
+
+    moveLeft() {
+
     }
 }
